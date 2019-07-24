e22498f4f8d511118ed834bead67ff2c1e3dfd68
BenStoever <stoever@bioinfweb.info>
2019-07-24 15:29:40 +0000

    Copyright information added to core project.

--- main/info.bioinfweb.jtreess.core/src/info/bioinfweb/jtreess/document/selector/Selector.java
+++ main/info.bioinfweb.jtreess.core/src/info/bioinfweb/jtreess/document/selector/Selector.java
@@ -1,7 +1,28 @@
+/*
+ * JTreeSS - A Java library for reading and evaluating TreeSS documents
+ * Copyright (C) 2019 Ben Stöver, Charlotte Schmitt
+ * <http://bioinfweb.info/JTreeSS>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package info.bioinfweb.jtreess.document.selector;
+
 
 import info.bioinfweb.jtreess.document.AbstractDocumentElement;
 import info.bioinfweb.jtreess.document.DocumentElement;
+
+
 
 public abstract class Selector extends AbstractDocumentElement {
 	public static enum SelectorType {
