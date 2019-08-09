11065244d63fe84b53823f10cf6d6fe6d62cc615
CharlotteSchmitt <CharlotteSchmitt@bioinfweb.info>
2019-08-09 10:25:38 +0000

    Added language package and filled it with various classes to implement the data structure for semantic analysis.

---
+++ main/info.bioinfweb.jtreess.core/src/info/bioinfweb/jtreess/language/ParamListEntry.java
@@ -0,0 +1,45 @@
+/*
+ * JTreeSS - A Java library for reading and evaluating TreeSS documents
+ * Copyright (C) 2019 Ben Stöver, Charlotte Schmitt
+ * <http://bioinfweb.info/JTreeSS>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.jtreess.language;
+
+
+public class ParamListEntry {
+	private Type paramType;
+	private Value value; 
+
+	
+	public Type getParamType() {
+		return paramType;
+	}
+
+	
+	public void setParamType(Type paramType) {
+		this.paramType = paramType;
+	}
+
+	
+	public Value getValue() {
+		return value;
+	}
+
+	
+	public void setValue(Value value) {
+		this.value = value;
+	}
+}
