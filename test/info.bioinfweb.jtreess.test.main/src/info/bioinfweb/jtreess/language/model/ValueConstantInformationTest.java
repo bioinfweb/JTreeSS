cc410c3dca50e2e40498b0af0828bd55b2537d39
BenStoever <stoever@bioinfweb.info>
2019-08-30 13:26:54 +0000

    ValueConstantInformationTest finished.

--- test/info.bioinfweb.jtreess.test.main/src/info/bioinfweb/jtreess/language/model/ValueConstantInformationTest.java
+++ test/info.bioinfweb.jtreess.test.main/src/info/bioinfweb/jtreess/language/model/ValueConstantInformationTest.java
@@ -1,6 +1,10 @@
 package info.bioinfweb.jtreess.language.model;
 
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 
 import javax.xml.bind.JAXBException;
@@ -8,6 +12,7 @@ import javax.xml.transform.stream.StreamSource;
 
 import org.junit.Test;
 
+import info.bioinfweb.jtreess.execute.RuntimeValue;
 import info.bioinfweb.jtreess.test.JTreeSSTestTools;
 
 
@@ -17,5 +22,23 @@ public class ValueConstantInformationTest {
 	public void test_Unmashalling() throws JAXBException {
 		ValueConstantInformation constantInformation = JTreeSSTestTools.createJAXBContext().createUnmarshaller().unmarshal(
 				new StreamSource(new File("data/language/constants/blue.xml")), ValueConstantInformation.class).getValue();
+		
+		assertEquals(RuntimeValue.parseRuntimeValue("#0000FF", RuntimeType.COLOR), constantInformation.getValue());
+		assertEquals("1.0", constantInformation.getIntroductoryVersion());
+		assertNull(constantInformation.getRemovingVersion());
+		
+		assertEquals(1, constantInformation.getExamples().size());
+		Example example = constantInformation.getExamples().get(0);
+		assertTrue("Code example \"" + example.getCode() + "\" was not expected.", 
+				example.getCode().matches("\\s*branch\\s*\\{\\s*line-color:\\s*blue\\;\\s*\\}\\s*"));
+		assertEquals("Sets the line color of all branches in a tree to blue.", example.getDescription());
+		assertEquals("Tree.nexml", example.getTreeURL());
+		assertEquals("Tree_blue_branches.svg", example.getResultImageURL());
+		
+		assertEquals(1, constantInformation.getSupportedSoftware().size());
+		SupportedSoftwareEntry entry = constantInformation.getSupportedSoftware().get(0);
+		assertEquals("TreeGraph 2", entry.getName());
+		assertEquals(1, entry.getSupportedVersionIntervals().size());
+		assertEquals(entry.getSupportedVersionIntervals().get(0), new SoftwareVersionInterval("2.14.0"));
 	}
 }
