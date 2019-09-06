c503e23a31260340737e120da0c359b92e94d5d5
BenStoever <stoever@bioinfweb.info>
2019-09-06 09:27:02 +0000

    Initial drafts for ApplicationDataProvider and TreeSSProcessor added.

---
+++ main/info.bioinfweb.jtreess.core/src/info/bioinfweb/jtreess/TreeSSProcessor.java
@@ -0,0 +1,91 @@
+/*
+ * JTreeSS - A Java library for reading and evaluating TreeSS documents
+ * Copyright (C) 2019 Ben Stöver, Charlotte Schmitt
+ * <http://bioinfweb.info/JTreeSS>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.jtreess;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import info.bioinfweb.commons.appversion.ApplicationType;
+import info.bioinfweb.commons.appversion.ApplicationVersion;
+import info.bioinfweb.jtreess.document.Document;
+import info.bioinfweb.jtreess.execute.ApplicationDataProvider;
+import info.bioinfweb.jtreess.reader.SyntaxTreeListener;
+import info.bioinfweb.jtreess.reader.parser.TreeSSLexer;
+import info.bioinfweb.jtreess.reader.parser.TreeSSParser;
+
+
+
+public class TreeSSProcessor {
+	private static TreeSSProcessor firstInstance = null;
+	
+	private ApplicationVersion version = new ApplicationVersion(0, 0, 0, 132, ApplicationType.ALPHA); 
+	
+	
+	private TreeSSProcessor() {
+		super();
+	}
+
+
+	public static TreeSSProcessor getInstance() {
+		if (firstInstance == null) {
+			firstInstance = new TreeSSProcessor();
+		}
+		return firstInstance;
+	}
+	
+	
+	public ApplicationVersion getVersion() {
+		return version;
+	}
+
+
+	public Document readTreeSS(String fileName) throws IOException {
+		return readTreeSS(CharStreams.fromFileName(fileName));
+	}
+	
+	
+	public Document readTreeSS(InputStream stream) throws IOException {
+		return readTreeSS(CharStreams.fromStream(stream));
+	}
+	
+	
+	public Document readTreeSS(InputStream stream, Charset charset) throws IOException {
+		return readTreeSS(CharStreams.fromStream(stream, charset));
+	}
+	
+	
+	private Document readTreeSS(CharStream charStream) {
+		SyntaxTreeListener listener = new SyntaxTreeListener();
+		new ParseTreeWalker().walk(listener, new TreeSSParser(new CommonTokenStream(new TreeSSLexer(charStream))).document());
+		//TODO Does the character stream or any other resource need to be closed or released?
+		return listener.getDocument();  //TODO Perform semantic analysis before returning the document
+	}
+	
+	
+	public <N> void applyDocument(Document document, ApplicationDataProvider<N> dataProvider) {
+		
+	}
+}
---
+++ main/info.bioinfweb.jtreess.core/src/info/bioinfweb/jtreess/execute/ApplicationDataProvider.java
@@ -0,0 +1,51 @@
+/*
+ * JTreeSS - A Java library for reading and evaluating TreeSS documents
+ * Copyright (C) 2019 Ben Stöver, Charlotte Schmitt
+ * <http://bioinfweb.info/JTreeSS>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.jtreess.execute;
+
+
+import info.bioinfweb.jtreess.TreeSSProcessor;
+
+
+
+/**
+ * This interface needs to be implemented by applications that want to make use of <i>JTreeSS</i>. It contains methods to provide information
+ * necessary to process a <i>TreeSS</i> document, e.g., on the tree topology, metadata values or size information.
+ * <p>
+ * The actual formatting of the tree is done by multiple calls of {@link #setFormat(Object, String, RuntimeValue)}. Implementations of this
+ * interface need to be passed to {@link TreeSSProcessor#applyDocument(info.bioinfweb.jtreess.document.Document, ApplicationDataProvider)}.
+ * 
+ * @author Ben St&ouml;ver
+ *
+ * @param <N> the tree node type used within the application
+ */
+public interface ApplicationDataProvider<N> {
+	public N getRoot();
+	
+	public int getChildCount(N parent);
+	
+	public N getChild(N parent, int index);
+	//TODO Should there also be a method getParent(N child)?
+	
+	public double getNumericMetadataValue(N node);  //TODO Add parameter for metadata path. Should data structures from JPhyloIO be used here or respective classes from there be moved to commons?
+	//TODO Are other types of metadata values necessary? Should these even be generic to use theme, e.g., in external function implementations?
+	
+	//TODO Add methods to convert between units/gather size information.
+	
+	public void setFormat(N node, String propertyName, RuntimeValue value); 
+}
