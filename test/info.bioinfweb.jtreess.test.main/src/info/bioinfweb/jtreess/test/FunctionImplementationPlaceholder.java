7b79d50ca75fb86940c05fa18e1049fa92642c92
BenStoever <stoever@bioinfweb.info>
2019-09-17 09:43:29 +0000

    Test project adjusted to recent changes.

--- test/info.bioinfweb.jtreess.test.main/src/info/bioinfweb/jtreess/test/FunctionImplementationPlaceholder.java
+++ test/info.bioinfweb.jtreess.test.main/src/info/bioinfweb/jtreess/test/FunctionImplementationPlaceholder.java
@@ -1,10 +1,17 @@
 package info.bioinfweb.jtreess.test;
 
 
+import java.util.List;
+
+import info.bioinfweb.jtreess.execute.ApplicationDataProvider;
+import info.bioinfweb.jtreess.execute.RuntimeValue;
 import info.bioinfweb.jtreess.execute.implementation.FunctionImplementation;
 
 
 
 public class FunctionImplementationPlaceholder implements FunctionImplementation {
-
+	@Override
+	public <N> RuntimeValue execute(List<RuntimeValue> parameters, N node, List<Integer> nodeIndices, ApplicationDataProvider<N> dataProvider) {
+		return null;
+	}
 }
