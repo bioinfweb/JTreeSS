25cea87e20cfce5a95ad0ad80efb2aedc388f400
BenStoever <stoever@bioinfweb.info>
2019-09-18 14:32:07 +0000

    JTreeSSTestTools now delegates to new method of LanguageDefinitionReader. LanguageDefinitionReaderTest added.

---
+++ test/info.bioinfweb.jtreess.test.main/src/info/bioinfweb/jtreess/language/io/LanguageDefinitionReaderTest.java
@@ -0,0 +1,48 @@
+package info.bioinfweb.jtreess.language.io;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.Map;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Test;
+
+import info.bioinfweb.jtreess.language.LanguageDefinitions;
+import info.bioinfweb.jtreess.language.model.EnumerationTypeInformation;
+import info.bioinfweb.jtreess.language.model.FunctionPseudoFunctionInformation;
+import info.bioinfweb.jtreess.language.model.PropertyInformation;
+import info.bioinfweb.jtreess.language.model.SimpleSelectorInformation;
+import info.bioinfweb.jtreess.language.model.SoftwareInformation;
+import info.bioinfweb.jtreess.language.model.UnitInformation;
+import info.bioinfweb.jtreess.language.model.ValueConstantInformation;
+
+
+
+public class LanguageDefinitionReaderTest {
+	private <I> void assertMap(Class<I> infoClass, Map<String, I> map, String... expectedNames) {
+		assertEquals(expectedNames.length, map.size());
+		
+		for (String name : expectedNames) {
+			assertTrue(map.containsKey(name));
+			assertTrue(infoClass.isInstance(map.get(name)));
+		}
+	}
+	
+	
+	@Test
+	public void test_readDefinitions() throws JAXBException {
+		LanguageDefinitions definitions = new LanguageDefinitionReader().readDefinitions(new File("data/language"));
+		
+		assertMap(ValueConstantInformation.class, definitions.getConstantsInformation(), "blue");
+		assertMap(FunctionPseudoFunctionInformation.class, definitions.getFunctionInformation(), "matches");
+		assertMap(PropertyInformation.class, definitions.getPropertyInformation(), "font-size");
+		assertMap(SimpleSelectorInformation.class, definitions.getSimpleSelectorInformation(), "node");
+		assertMap(SoftwareInformation.class, definitions.getSoftwareInformation(), "TreeGraph_2");
+		assertMap(EnumerationTypeInformation.class, definitions.getEnumTypeInformation(), "text-align");
+		assertMap(UnitInformation.class, definitions.getUnitInformation(), "blu", "pt");
+	}
+}
--- test/info.bioinfweb.jtreess.test.main/src/info/bioinfweb/jtreess/test/JTreeSSTestTools.java
+++ test/info.bioinfweb.jtreess.test.main/src/info/bioinfweb/jtreess/test/JTreeSSTestTools.java
@@ -4,19 +4,12 @@ package info.bioinfweb.jtreess.test;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
-import info.bioinfweb.jtreess.language.model.EnumerationTypeInformation;
-import info.bioinfweb.jtreess.language.model.FunctionPseudoFunctionInformation;
-import info.bioinfweb.jtreess.language.model.PropertyInformation;
-import info.bioinfweb.jtreess.language.model.SimpleSelectorInformation;
-import info.bioinfweb.jtreess.language.model.SoftwareInformation;
-import info.bioinfweb.jtreess.language.model.UnitInformation;
-import info.bioinfweb.jtreess.language.model.ValueConstantInformation;
+import info.bioinfweb.jtreess.language.io.LanguageDefinitionReader;
 
 
 
 public class JTreeSSTestTools {
 	public static JAXBContext createJAXBContext() throws JAXBException {
-		return JAXBContext.newInstance(SimpleSelectorInformation.class, PropertyInformation.class, ValueConstantInformation.class, 
-				EnumerationTypeInformation.class, FunctionPseudoFunctionInformation.class, SoftwareInformation.class, UnitInformation.class);
+		return LanguageDefinitionReader.createJAXBContext();
 	}
 }
